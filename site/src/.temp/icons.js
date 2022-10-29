98e79931f47d2c91503dda1990afa84aa22e935a
Steve Springett <steve@springett.us>
2022-10-28 22:45:22 -0500

    Renamed site to docs to accomidate gh pages

--- site/src/.temp/icons.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "touchiconMimeType": "image/png",
-  "faviconMimeType": null,
-  "precomposed": false,
-  "touchicons": [],
-  "favicons": []
-}
--- site/src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1667014793363
