d2445611ca2e14b5cb176bc12ec7c918b7566f0a
Steve Springett <steve@springett.us>
2022-10-31 10:28:59 -0500

    Add bmm info

--- site/src/pages/bom-maturity-model.vue
+++ site/src/pages/bom-maturity-model.vue
@@ -6,7 +6,29 @@
           <h1 class="text-4xl font-black border-none">
             BOM Maturity Model
           </h1>
-          <p>SCVS is developing a model in which bill of materials can be evaluated for a wide range of capabilities. The model will allow users to:</p>
+          <h3>Coming soon...</h3>
+          <p>SCVS is developing a model in which bill of materials can be evaluated for a wide range of capabilities. The model consists of:</p>
+          <ul class="ml-10 list-disc">
+            <li>a formal taxonomy of different types of data possible in a bill of materials, independent of BOM format</li>
+            <li>a unique identifier, description, and other metadata about each item in the taxonomy</li>
+            <li>the level of complexity or difficulty in supporting different types of data</li>
+          </ul>
+
+          <p>The model can be used to evaluate:</p>
+          <ul class="ml-10 list-disc">
+            <li>Incoming BOMs adhere to organizational policy by supporting the data required by various stakeholders</li>
+            <li>BOM generation and consumption tools</li>
+            <li>Current and future BOM formats against each other and their alignment to organizational requirements</li>
+          </ul>
+
+          <p>In addition to the taxonomy, the BOM Maturity Model also provides support for profiles, which group multiple
+          items in the model together along with weighted importance and other metadata. Organizations may create multiple
+          profiles, each specific to various stakeholder requirements, or may opt for a global profile where a single organization
+          profile contains all of the required items of the model. This will facilitate:</p>
+          <ul class="ml-10 list-disc">
+            <li>The creation of a new breed of tools which evaluate BOMs against various profiles so that end users may know what types of analysis can be performed on them</li>
+            <li>The adoption of organizational policy, defined in profiles, for what is acceptable and not acceptable for various use cases</li>
+          </ul>
         </div>
       </div>
     </div>
